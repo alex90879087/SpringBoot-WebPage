aa5665ea3d4bfe2edf0278e249424db77f72028b
Alex Shih <ashi5226@uni.sydney.edu.au>
2023-04-07 15:46:34 +1000

    property test, need to confirm if that I need to test Integer.Max_Value of times or the shoppingbasket can add Integer.Max_Value number of items into the cart

--- app/src/test/java/au/edu/sydney/soft3202/task1/ShoppingBasketPropertyTest.java
+++ app/src/test/java/au/edu/sydney/soft3202/task1/ShoppingBasketPropertyTest.java
@@ -1,14 +1,20 @@
 package au.edu.sydney.soft3202.task1;
 
+import net.jqwik.api.ForAll;
 import net.jqwik.api.Property;
+import net.jqwik.api.constraints.IntRange;
 
 import static org.assertj.core.api.Assertions.assertThat;
-
+import static org.junit.jupiter.api.Assertions.*;
 public class ShoppingBasketPropertyTest {
 
+
     @Property
-    void getValueEmptyPropertyTest() {
-        ShoppingBasket sb = new ShoppingBasket();
-        assertThat(sb.getValue()).isEqualTo(null);
+    void getValueEmptyPropertyTest(@ForAll @IntRange(min = 1, max = Integer.MAX_VALUE) int n) {
+        ShoppingBasket sut = new ShoppingBasket();
+        String name = "apple";
+        sut.addItem(name, n);
+        sut.removeItem(name, n);
+        assertNull(sut.getValue());
     }
 }
